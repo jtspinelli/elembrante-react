f7f860fc2932ed81db4cd88ba7f05b6515b50117
Vinícius <vinicius@easypro.com.br>
2023-02-09 13:21:53 -0300

    add mui

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,7 +1,12 @@
+import { Button, Typography } from "@mui/material";
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+
 const MainPage: React.FC = () => {
     return (
         <>
-            <h1> Hello from MainPage</h1>
+            <Typography variant="h1"> Hello from MainPage </Typography>
+
+            <Button variant="contained"> <AccessAlarmIcon /> MUI Testing</Button>
         </>
     )
 }
