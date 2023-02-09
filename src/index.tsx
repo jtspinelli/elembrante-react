fc273ed2cd91c6b86588bd4d5f7fc7c4612129cb
Vinícius <vinicius@easypro.com.br>
2023-02-09 13:06:27 -0300

    add react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+]);
+
 root.render(
-    <App />
+    <RouterProvider router={router} />
 );
