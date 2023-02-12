b26dedd43855a465119d0960b06572791e10a167
jtspinelli <contato@jonathanspinelli.com>
2023-02-12 00:13:27 -0300

    change theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,34 @@
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { createBrowserRouter } from 'react-router-dom';
-import { RouterProvider } from 'react-router';
-import MainPage from './pages/Main/MainPage';
-import { Provider } from 'react-redux';
-import store, { persistor } from './feature/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router";
+import MainPage from "./pages/Main/MainPage";
+import { Provider } from "react-redux";
+import store, { persistor } from "./feature/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { createTheme } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
-const router = createBrowserRouter([
-  { path: "/", element: <MainPage /> },
-]);
+const router = createBrowserRouter([{ path: "/", element: <MainPage /> }]);
+
+const meuTema = createTheme({
+  palette: {
+    primary: {
+      main: "#009688",
+    },
+  },
+});
 
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <RouterProvider router={router} />
+      <ThemeProvider theme={meuTema}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </PersistGate>
   </Provider>
 );
