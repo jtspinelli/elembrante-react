b21f4ca7388854ad1c88457ccbab6446c6cf5c02
jtspinelli <contato@jonathanspinelli.com>
2023-02-12 00:16:12 -0300

    export selectById selector

--- src/feature/lembreteSlice.tsx
+++ src/feature/lembreteSlice.tsx
@@ -3,20 +3,22 @@ import Lembrete from "./Lembrete";
 import { RootState } from "./store";
 
 const lembreteAdapter = createEntityAdapter<Lembrete>({
-    selectId: (lembrete) => lembrete.id
+  selectId: (lembrete) => lembrete.id,
 });
 
 const lembreteSlice = createSlice({
-    name: "lembretes",
-    initialState: lembreteAdapter.getInitialState(),
-    reducers: {
-        addLembrete: lembreteAdapter.addOne,
-        removeLembrete: lembreteAdapter.removeOne,
-        updateLembrete: lembreteAdapter.updateOne
-    }
+  name: "lembretes",
+  initialState: lembreteAdapter.getInitialState(),
+  reducers: {
+    addLembrete: lembreteAdapter.addOne,
+    removeLembrete: lembreteAdapter.removeOne,
+    updateLembrete: lembreteAdapter.updateOne,
+  },
 });
 
-export const { selectAll } = lembreteAdapter.getSelectors((state: RootState) => state.lembretesReducer);
+export const { selectAll, selectById } = lembreteAdapter.getSelectors(
+  (state: RootState) => state.lembretesReducer
+);
 export const { addLembrete } = lembreteSlice.actions;
 
 export default lembreteSlice;
