76dd95e7374fcf4340bfb41217fba8fd803a2f08
jtspinelli <contato@jonathanspinelli.com>
2023-02-12 00:20:08 -0300

    add theme

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import MainPage from "./pages/Main/MainPage";
 import { Provider } from "react-redux";
 import store, { persistor } from "./feature/store";
 import { PersistGate } from "redux-persist/integration/react";
-import { createTheme } from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
+import theme from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -15,18 +15,10 @@ const root = ReactDOM.createRoot(
 
 const router = createBrowserRouter([{ path: "/", element: <MainPage /> }]);
 
-const meuTema = createTheme({
-  palette: {
-    primary: {
-      main: "#009688",
-    },
-  },
-});
-
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <ThemeProvider theme={meuTema}>
+      <ThemeProvider theme={theme}>
         <RouterProvider router={router} />
       </ThemeProvider>
     </PersistGate>
---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#009688",
+    },
+  },
+});
+
+export default theme;
