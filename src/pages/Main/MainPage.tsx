a78dba0f420ba8a1cfc5a41ddfc1474ecea9684a
jtspinelli <contato@jonathanspinelli.com>
2023-02-12 14:31:44 -0300

    styling

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,6 +1,5 @@
 import {
   Box,
-  Button,
   Card,
   CardActions,
   CardContent,
@@ -9,17 +8,22 @@ import {
 } from "@mui/material";
 import { RootState } from "../../feature/store";
 import { useSelector } from "react-redux";
+import { selectAll } from "../../feature/lembreteSlice";
 import AppBar from "../components/appBar/AppBar";
 import DrawerHeader from "../components/sideBar/styles";
 import SideBar from "../components/sideBar/SideBar";
 import Main from "./styles";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
+import Lembrete from "../../feature/Lembrete";
 
 const MainPage: React.FC = () => {
   const { open, width } = useSelector(
     (state: RootState) => state.sideBarReducer
   );
+
+  const lembretes: Lembrete[] = useSelector(selectAll);
+
   return (
     <Box sx={{ display: "flex" }}>
       <AppBar />
@@ -27,29 +31,35 @@ const MainPage: React.FC = () => {
       <SideBar />
 
       <Main open={open} width={width}>
-        <DrawerHeader />
-        <Typography variant="h3">Meus lembretes</Typography>
+        <Box id="container">
+          <DrawerHeader />
+          <Typography variant="h3">Meus lembretes</Typography>
 
-        <Card sx={{ maxWidth: 345 }}>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Primeiro
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Fazer tal e tal coisa.
-            </Typography>
-          </CardContent>
-          <CardActions
-            sx={{ display: "flex", justifyContent: "space-between" }}
-          >
-            <IconButton aria-label="delete" color="error">
-              <DeleteIcon />
-            </IconButton>
-            <IconButton aria-label="delete">
-              <EditIcon />
-            </IconButton>
-          </CardActions>
-        </Card>
+          <Box id="cards-container">
+            {lembretes.map((lembrete: Lembrete) => (
+              <Card sx={{ maxWidth: 345 }}>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {lembrete.descricao}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {lembrete.detalhamento}
+                  </Typography>
+                </CardContent>
+                <CardActions
+                  sx={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <IconButton aria-label="delete" color="error">
+                    <DeleteIcon />
+                  </IconButton>
+                  <IconButton aria-label="delete">
+                    <EditIcon />
+                  </IconButton>
+                </CardActions>
+              </Card>
+            ))}
+          </Box>
+        </Box>
       </Main>
     </Box>
   );
--- src/pages/Main/styles.tsx
+++ src/pages/Main/styles.tsx
@@ -1,18 +1,36 @@
 import { styled } from "@mui/material";
 
+const spacing = "20px";
+
 const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
   open?: boolean;
   width: number;
 }>(({ theme, open, width }) => ({
   flexGrow: 1,
+  display: "flex",
+  justifyContent: "center",
   padding: theme.spacing(3),
-  transition: theme.transitions.create("margin", {
+  "& #container": {
+    width: "100%",
+    maxWidth: "1100px",
+
+    "& #cards-container": {
+      display: "flex",
+      flexWrap: "wrap",
+      gap: spacing,
+      "& .MuiCard-root": {
+        width: `calc(33.33% - (${spacing} * 2 / 3))`,
+        maxWidth: "100%",
+      },
+    },
+  },
+  transition: theme.transitions.create(["margin", "width"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   marginLeft: `-${width}px`,
   ...(open && {
-    transition: theme.transitions.create("margin", {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
