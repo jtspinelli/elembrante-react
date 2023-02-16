b0b8c3137669e28193b61e6350a93c03c65cf541
jtspinelli <contato@jonathanspinelli.com>
2023-02-15 21:08:45 -0300

    [CRUD]: Implement Creat logic

--- src/pages/components/form/Form.tsx
+++ src/pages/components/form/Form.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { BoxDetalhamento, BoxTitulo, CustomPaper, Placeholder, TextBoxDetalhamento, TextBoxTitulo } from './styles';
+import { v4 as uuid } from 'uuid';
+import { addLembrete } from '../../../feature/lembreteSlice';
+import { useDispatch } from 'react-redux';
+import Lembrete from '../../../feature/Lembrete';
 
 const Form: React.FC = () => {
 	const [ formExpanded, _setFormExpanded ] = useState<boolean>(false);
@@ -7,6 +11,8 @@ const Form: React.FC = () => {
 	const formIsExpanded = useRef(formExpanded);
 	const detalhamentoTextbox = useRef<HTMLDivElement>();
 	const tituloTextbox = useRef<HTMLDivElement>();
+
+	const dispatch = useDispatch();
 
 	useEffect(() => document.body.addEventListener('click', collapseFormIfOutsideClick), []);	
 	
@@ -20,12 +26,25 @@ const Form: React.FC = () => {
 
 		if(clickedOutsideForm && formIsExpanded.current){
 			if(detalhamentoTextbox.current?.innerText.length || tituloTextbox.current?.innerText.length){
-				//createLembrete
+				createLembrete();
 				return;
 			}
 			resetForm();
 		}
 	};
+
+	function createLembrete(){
+		const lembrete: Lembrete = {
+			id: uuid(),
+			criadoEm: new Date(),
+			descricao: tituloTextbox.current?.innerText.length ? tituloTextbox.current?.innerText : null,
+			detalhamento: detalhamentoTextbox.current?.innerText.length ? detalhamentoTextbox.current?.innerText : null
+		};
+
+		dispatch(addLembrete(lembrete));
+
+		resetForm();
+	}
 
 	function collapseForm(){
 		setFormExpanded(false);
