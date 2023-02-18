e9d67da9a4d76dff487ab271d5e7c57359c16e10
jtspinelli <contato@jonathanspinelli.com>
2023-02-17 22:22:40 -0300

    add EditModalSlice

--- src/feature/store.ts
+++ src/feature/store.ts
@@ -1,9 +1,9 @@
-import React from 'react';
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/es/storage';
 import lembreteSlice from './lembreteSlice';
 import sideBarSlice from './sideBarSlice';
+import editModalSlice from './editModalSlice';
 
 const persistConfig = {
 	key: 'root',
@@ -16,6 +16,7 @@ const store = configureStore({
 	reducer: {
 		lembretesReducer: persistedReducer,
 		sideBarReducer: sideBarSlice.reducer,
+		editModalReducer: editModalSlice.reducer
 	},
 	middleware: (getDefaultMiddleware) =>
 		getDefaultMiddleware({
