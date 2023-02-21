491c8687685252c3abb4f001271ab0f395a0e7ca
jtspinelli <contato@jonathanspinelli.com>
2023-02-21 15:27:00 -0300

    [REGISTER]: add form validations

--- src/pages/components/register/Register.tsx
+++ src/pages/components/register/Register.tsx
@@ -1,33 +1,70 @@
-import React, { useRef, useState } from 'react';
-import { Button, TextField, Typography } from '@mui/material';
+/* eslint-disable no-useless-escape */
+import React, { useState } from 'react';
+import { hasLetterRegex, hasNumberRegex, hasSpecialCharRegex, senhaRegex } from '../../../helpers/regex';
+import { Button, List, ListItem, TextField, Typography } from '@mui/material';
 import { Actions, Form } from '../login/styles';
 import { Link } from 'react-router-dom';
 
 const Register: React.FC = () => {
-	const nome = useRef<HTMLInputElement>();
+	const [nome, setNome] = useState<string>('');
 	const [ username, setUsername] = useState<string>('');
-	const senha = useRef<HTMLInputElement>();
-	const confirmSenha = useRef<HTMLInputElement>();
+	const [ senha, setSenha ] = useState<string>('');
+	const [ confirmSenha, setConfirmSenha] = useState<string>('');
+	const [ nomePass, setNomePass ] = useState<boolean>();
+	const [ usernamePass, setUsernamePass ] = useState<boolean>();
+	const [ senhaPass, setSenhaPass ] = useState<boolean>();
+	const [ senhaLengthPass, setSenhaLengthPass ] = useState<boolean>();
+	const [ senhaHasLetterPass, setSenhaHasLetterPass ] = useState<boolean>();
+	const [ senhaHasNumberPass, setSenhaHasNumberPass ] = useState<boolean>();
+	const [ senhaHasSpecialCharPass, setSenhaHasSpecialCharPass ] = useState<boolean>();
+	const [ confirmSenhaPass, setConfirmSenhaPass ] = useState<boolean>();
 
 	function criarConta(e: React.FormEvent<HTMLDivElement>){
 		e.preventDefault();
 
-		console.log(nome.current?.value);
-		console.log(username);
-		console.log(senha.current?.value);
-		console.log(confirmSenha.current?.value);
+		if(!nomePass || !usernamePass || !senhaPass || !confirmSenhaPass) return;
+
+		alert('create!');
+	}
+
+	function checkNome(){
+		setNomePass(nome.length > 2);
+	}
+
+	function checkUsername(){
+		setUsernamePass(username.length > 2);
+	}
+
+	function checkSenha(){
+		const senhaMatch = senha.match(senhaRegex);
+		setSenhaLengthPass(senha.length > 3);
+		setSenhaHasLetterPass(senha.match(hasLetterRegex) !== null);
+		setSenhaHasNumberPass(senha.match(hasNumberRegex) !== null);
+		setSenhaHasSpecialCharPass(senha.match(hasSpecialCharRegex) !== null);
+		checkConfirmSenha();
+		setSenhaPass(senhaMatch !== null && senhaMatch[0] === senha);
+	}
+
+	function checkConfirmSenha(){
+		setConfirmSenhaPass(confirmSenha === senha);
 	}
 
 	return (
 		<Form onSubmit={criarConta}>
 			<Typography variant='h4'> Criar conta </Typography>
 			<TextField 
+				onKeyUp={checkNome}
+				error={nomePass === false}
+				helperText={!nome.length && nomePass === false ? 'Preencha o campo' : nomePass === false ? 'Mínimo 3 dígitos.' : ''}
 				sx={{ margin: '40px 0 0' }}
 				label='Digite seu nome'
-				inputRef={nome}
+				onChange={(e) => setNome(e.target.value)}
 			/>
 
 			<TextField 
+				onKeyUp={checkUsername}
+				error={usernamePass === false}
+				helperText={!username.length && usernamePass === false ? 'Preencha o campo.' : usernamePass === false ? 'Mínimo 3 dígitos.' : ''}
 				sx={{ margin: '40px 0 0', input: { textTransform: 'lowercase' } }}
 				label='Nome de usuário'
 				value={username}
@@ -38,17 +75,38 @@ const Register: React.FC = () => {
 			/>
 
 			<TextField 
+				onKeyUp={checkSenha}
+				error={senhaPass === false}
 				sx={{ margin: '40px 0 0' }}
 				label='Senha'
-				inputRef={senha}
 				type='password'
+				onChange={(e) => setSenha(e.target.value)}
 			/>
+			{senhaPass === false && 
+				<List sx={{ fontSize: '0.75rem', color: '#f44336', paddingTop: '3px', li: { padding: '0 0 0 14px' } }}>
+					{!senha.length && senhaPass === false &&
+						<ListItem>Preencha o campo.</ListItem>
+					}
 
-			<TextField 
+					{senha.length > 0 && senhaPass === false &&
+						<>
+							<ListItem sx={{ color: senhaLengthPass ? 'green' : 'unset' }}> {senhaLengthPass && <span style={{marginRight: '5px'}}>✓</span>} Mínimo 4 dígitos.</ListItem>
+							<ListItem sx={{ color: senhaHasLetterPass ? 'green' : 'unset' }}> {senhaHasLetterPass && <span style={{marginRight: '5px'}}>✓</span>} Ao menos uma letra.</ListItem>
+							<ListItem sx={{ color: senhaHasNumberPass ? 'green' : 'unset' }}> {senhaHasNumberPass && <span style={{marginRight: '5px'}}>✓</span>}Ao menos um número.</ListItem>
+							<ListItem sx={{ color: senhaHasSpecialCharPass ? 'green' : 'unset' }}>{senhaHasSpecialCharPass && <span style={{marginRight: '5px'}}>✓</span>} Ao menos um caractere especial.</ListItem>
+						</>
+					}					
+				</List>
+			}
+
+			<TextField
+				onKeyUp={checkConfirmSenha}
+				error={confirmSenhaPass === false}
+				helperText={!confirmSenha.length && confirmSenhaPass === false ? 'Preencha o campo.' : confirmSenhaPass === false ? 'Campos de senha devem ser iguais.' : ''}
 				sx={{ margin: '40px 0' }}
 				label='Repita a senha'
-				inputRef={confirmSenha}
 				type='password'
+				onChange={(e) => setConfirmSenha(e.target.value)}
 			/>
 
 			<Actions>
