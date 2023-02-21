0f43f777107146d483579849191a41e9c9a77ddb
jtspinelli <contato@jonathanspinelli.com>
2023-02-21 20:07:51 -0300

    [REGEX]: include . and _ in regex for username

--- src/helpers/regex.ts
+++ src/helpers/regex.ts
@@ -3,3 +3,4 @@ export const senhaRegex = /(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};
 export const hasLetterRegex = /(?=.*[a-zA-Z])[a-zA-Z]{1,}/;
 export const hasNumberRegex = /(?=.*[0-9])[0-9]{1,}/;
 export const hasSpecialCharRegex = /(?=.*[!@#$%^&*()_+\-=\[\]{};\':"\\|,.<>\/\?])[!@#$%^&*()_+\-=\[\]{};\':"\\|,.<>\/\?]{1,}/;
+export const usernameRegex = /[^a-z._]/gi;
