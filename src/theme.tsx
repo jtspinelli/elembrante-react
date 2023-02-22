d14dd46f3499ba72cb42b74fe0e55c8661c72b3b
jtspinelli <contato@jonathanspinelli.com>
2023-02-21 22:54:42 -0300

    [THEME]: add mui theme colors customization

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,14 +1,39 @@
 import { createTheme } from '@mui/material';
 
+const elembranteOriginalGreen = '#4ab354';
+const muiBlue = '#1976d2';
+const using = '#ca792e';
+
+declare module '@mui/material/styles' {
+	interface Theme {
+	  appIcon: {
+		default: string;
+	  };
+	}
+
+	interface ThemeOptions {
+	  appIcon?: {
+		default?: string;
+	  };
+	}
+  }
+
 const theme = createTheme({
 	palette: {
 		primary: {
-			main: '#1976d2',
+			main: using,
 		},
 		error: {
 			main: '#f44336',
 		},
+		secondary: {
+			main: muiBlue
+		}
 	},
+	appIcon: {
+		default: using
+	}
 });
+
 
 export default theme;
