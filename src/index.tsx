585883566dfe19c4ccf957b2fd273e9ea768c7f4
jtspinelli <contato@jonathanspinelli.com>
2023-02-25 13:06:41 -0300

    [OAUTH GOOGLE]: change lib; use @react-oauth/google

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,18 @@ import { Provider } from 'react-redux';
 import store, { persistor } from './app/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-	<Provider store={store}>
-		<PersistGate loading={null} persistor={persistor}>
-			<App />
-		</PersistGate>
-	</Provider>
+	<GoogleOAuthProvider clientId='***REMOVED***'>
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<App />
+			</PersistGate>
+		</Provider>
+	</GoogleOAuthProvider>
 );
